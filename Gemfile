287b6e21030ad38ec7b8f3c22e136a21b15f7f71
Eric <eric.m.lombardo@gmail.com>
2021-04-05 18:18:20 -0400

    Build form to create song, artist, genre,  notes

--- Gemfile
+++ Gemfile
@@ -41,6 +41,8 @@ end
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
+  # Call 'binding.pry' to start a pry session
+  gem 'pry'
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
   gem 'rspec-rails', '~> 3.7'
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,27 @@
 class Song < ActiveRecord::Base
-  # add associations here
+  belongs_to :artist 
+  belongs_to :genre 
+  has_many :notes
+
+
+  def artist_name=(artist_name)
+    self.artist = Artist.find_or_create_by(name: artist_name)
+  end
+
+  def artist_name
+    self.artist ? self.artist.name : nil
+  end
+
+  def notes=(notes) # take any notes and build them already linked to song
+    notes.each {|n| self.notes.build(content: n)} unless notes.all?("")
+  end
 end
+
+# Had this, but it is taken care of with active record
+# def genre_id=(genre_id)
+#   self.genre = Genre.find_by_id(genre_id)
+# end
+
+# def genre_id
+#   self.genre ? self.genre.name : nil 
+# end
